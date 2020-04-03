19a3440d981844f3fe8a302baef961858b5e78b4
saarakhk <saara.vallik@khk.ee>
2020-04-03 12:35:16 +0300

    update

--- node-express-realworld-example-app-master/routes/api/users.js
+++ node-express-realworld-example-app-master/routes/api/users.js
@@ -137,26 +137,6 @@ router.put('/user', auth.required, function(req, res, next){
   }).catch(next);
 });
 
-router.post('/users/login', function(req, res, next){
-  if(!req.body.user.email){
-    return res.status(422).json({errors: {email: "can't be blank"}});
-  }
-
-  if(!req.body.user.password){
-    return res.status(422).json({errors: {password: "can't be blank"}});
-  }
-
-  passport.authenticate('local', {session: false}, function(err, user, info){
-    if(err){ return next(err); }
-
-    if(user){
-      user.token = user.generateJWT();
-      return res.json({user: user.toAuthJSON()});
-    } else {
-      return res.status(422).json(info);
-    }
-  })(req, res, next);
-});
 /**
  * @swagger
  * /api/users/login:
@@ -201,6 +181,28 @@ router.post('/users/login', function(req, res, next){
  *       409:
  *         description: When the email is already in use
  */
+
+router.post('/users/login', function(req, res, next){
+  if(!req.body.user.email){
+    return res.status(422).json({errors: {email: "can't be blank"}});
+  }
+
+  if(!req.body.user.password){
+    return res.status(422).json({errors: {password: "can't be blank"}});
+  }
+
+  passport.authenticate('local', {session: false}, function(err, user, info){
+    if(err){ return next(err); }
+
+    if(user){
+      user.token = user.generateJWT();
+      return res.json({user: user.toAuthJSON()});
+    } else {
+      return res.status(422).json(info);
+    }
+  })(req, res, next);
+});
+
 router.post('/users', function(req, res, next){
   var user = new User();
 
